eb141fd1d5f7cce3c4310f8028d8d9cad30e8bf5
Drausin Wulsin <daedalus2718@gmail.com>
2018-09-24 14:54:40 -0400

    bump from 8 -> 16 librarians (#3)

--- terraform/env.sh
+++ terraform/env.sh
@@ -8,6 +8,6 @@ export TF_VAR_secrets_key_ring_name="libri"
 export TF_VAR_tf_secrets_key_name="libri-tf-secrets"
 
 # Libri
-export TF_VAR_n_librarians=8
+export TF_VAR_n_librarians=16
 export TF_VAR_librarian_disk_size=25
 export TF_VAR_public_port_start=30100
